8f65456331f1150d4966af659b858615ea72c97c
Alexandros Andreou <alex10791@gmail.com>
2018-10-27 12:57:57 +0300

    added wallet input, qr modules and generate wallet address qr

--- mobile-app/src/app/wallet/wallet.page.ts
+++ mobile-app/src/app/wallet/wallet.page.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletPage implements OnInit {
 
+  addresses = [
+    'address1',
+    'address2'
+  ]
+  selectedAddress;
+
   constructor() { }
 
   ngOnInit() {
