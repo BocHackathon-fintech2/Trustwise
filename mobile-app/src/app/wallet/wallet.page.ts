94639249c610e45ef3dd111c7f26da6e69bd567e
Alexandros Andreou <alex10791@gmail.com>
2018-10-27 17:38:10 +0300

    added support for web3

--- mobile-app/src/app/wallet/wallet.page.ts
+++ mobile-app/src/app/wallet/wallet.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { w3 } from '../web3/w3.js';
+import { Storage } from "@ionic/storage";
+// import { AndroidPermissions } from '@ionic-native/android-permissions';
 
 @Component({
   selector: 'app-wallet',
@@ -13,9 +16,38 @@ export class WalletPage implements OnInit {
   ]
   selectedAddress;
 
-  constructor() { }
+  constructor(
+    private storage: Storage) { }
 
-  ngOnInit() {
+    // private androidPermissions: AndroidPermissions
+
+    ngOnInit() {
+    // this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE);
+    // this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE);
+
+    // this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE).then(
+    //   result => console.log('Has permission?',result.hasPermission),
+    //   err => )
+    // );
+
+    // this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE).then(
+    //   result => console.log('Has permission?',result.hasPermission),
+    //   err => )
+    // );
+
+    this.addresses = w3.eth.accounts;
+    this.storage.get('account').then(account => {
+      if (account) {
+        this.selectedAddress = account;
+      }
+    });
+  }
+
+  selectAddress() {
+    console.log('In selectAddress');
+    console.log(this.selectedAddress);
+    this.storage.set('account', this.selectedAddress);
+    
   }
 
 }
--- mobile-app/src/app/web3/w3.js
+++ mobile-app/src/app/web3/w3.js
@@ -1,3 +1,4 @@
 import Web3 from 'web3';
 
-export const w3 = new Web3(new Web3.providers.HttpProvider("http://raspberrypi.local:8545"))
+// export const w3 = new Web3(new Web3.providers.HttpProvider("http://raspberrypi.local:8545"))
+export const w3 = new Web3(new Web3.providers.HttpProvider("http://192.168.88.160:8545"))
