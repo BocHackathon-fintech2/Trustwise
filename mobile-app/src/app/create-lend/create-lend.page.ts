a0e6dfb33cd4b49b9e316cf822f572fc6a35b387
Vlad Lupashevskyi <vlad@lupashevskyi.com>
2018-10-27 14:58:17 +0300

    introduce web3 + test web3 calls

--- mobile-app/src/app/create-lend/create-lend.page.ts
+++ mobile-app/src/app/create-lend/create-lend.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { w3 } from '../web3/w3.js';
 
 @Component({
   selector: 'app-create-lend',
@@ -10,6 +11,12 @@ export class CreateLendPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  test() {    
+    console.log(w3.eth.accounts);
+    w3.eth.sendTransaction({from: "0xe414D4b9b33C5C00fc173dCc8f07d7607faFA6c7", to: "0xe414D4b9b33C5C00fc173dCc8f07d7607faFA6c7", value: 100})
+        
   }
 
 }
