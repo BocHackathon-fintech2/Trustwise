bee26ba68a3f976266fa0a047867f1ba50c66884
Nicolas Tsagarides <nicexe@gmail.com>
2018-10-28 11:32:39 +0200

    complete system services in backend setup script

--- backend/backend-setup.sh
+++ backend/backend-setup.sh
@@ -8,6 +8,11 @@ chmod +x /usr/bin/parity
 mkdir -p /home/pi/.local/share/io.parity.ethereum/
 parity account new
 cp parity.service /etc/systemd/system/parity.service
+chmod 644 /etc/systemd/system/parity.service
+cp vault-manager.service /lib/systemd/system/vault-manager.service
+chmod 644 /lib/systemd/system/vault-manager.service
+systemctl daemon-reload
 systemctl enable parity.service
+systemctl enable vault-manager
 cp config.toml ~/.local/share/io.parity.ethereum/config.toml
 cp dev-chain.json > /home/pi/.local/share/io.parity.ethereum/dev-chain.json
